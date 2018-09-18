cd9e29c9ddd742bba4b7d94a928727f1b001af9b
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-17 20:16:36 -0500

    selectors

---
+++ scripts.js
@@ -0,0 +1,5 @@
+const player = document.querySelector(".player");
+const photo = document.querySelector(".photo");
+const context = photo.getContext("2d");
+const photos = document.querySelector(".photos");
+const snap = document.querySelector(".snap");

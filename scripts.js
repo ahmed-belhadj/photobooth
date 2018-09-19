45ba826525927a092ca5f8c1388c76e9681ae9bd
Ahmed Belhadj <mr.ahmedbelhadj@gmail.com>
2018-09-18 22:09:28 -0500

    basic displayUserCamera()

--- scripts.js
+++ scripts.js
@@ -3,3 +3,20 @@ const photo = document.querySelector(".photo");
 const context = photo.getContext("2d");
 const photos = document.querySelector(".photos");
 const snap = document.querySelector(".snap");
+
+function displayUserCamera() {
+  photo.height = player.videoHeight;
+  photo.width = player.videoWidth;
+  navigator.mediaDevices
+    .getUserMedia({ video: true, audio: false })
+    .then(mediaStream => {
+      player.src = window.URL.createObjectURL(mediaStream);
+      player.play();
+    })
+    .catch(error => {
+      console.error("Error: " + error);
+      alert("Error: " + error);
+    });
+}
+
+displayUserCamera();
